f29f62ebbc323e91896ac2fa96f776c8ba8dbdbd
wyf9 <wyf9@wyf9.top>
2025-09-07 14:55:56 +0800

    log & check_available

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from short-domain-find!")
+from config import config as c
+from utils import *
+import asyncio
 
+async def main():
+    suffs = await get_suffixs(c.max_length)
+    print(f'Got suffixs: {suffs}')
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    asyncio.run(main())
